d1a115c41add1fff463b4dd67534a3731d3d2d18
Reddanna Kotte <reddy.iphonedev@gmail.com>
2025-10-06 17:48:02 +0530

    Added new method asyncDownload, which supports generic API calls

--- Sources/APIPackageManager/APIManager.swift
+++ Sources/APIPackageManager/APIManager.swift
@@ -13,10 +13,28 @@ public struct APIManager {
     
     public init() {
     }
-    public func getData<T:Decodable>(from url: URL, type:[T].Type)-> AnyPublisher<[T], Error> {
+    
+    public func getData<T: Decodable>(from url: URL, type:[T].Type)-> AnyPublisher<[T], Error> {
         URLSession.shared.dataTaskPublisher(for: url)
             .map(\.data)
             .decode(type: [T].self, decoder: JSONDecoder())
             .eraseToAnyPublisher()
+    }
+    
+    public func asyncDownload<T: Decodable>(from url: URL, type:[T].Type) async throws
+                                                        -> ([T]?, Error?) {
+       
+        do {
+            let (responseData, response) = try await URLSession.shared.data(from: url)
+            guard let resp = response as? HTTPURLResponse, resp.statusCode == 200 else {
+               throw URLError(.badURL)
+            }
+            let json = try JSONDecoder().decode([T].self, from: responseData)
+            return (json, nil)
+            
+        }catch {
+            print("Error: \(error)")
+        }
+         return (nil, nil)
     }
 }
